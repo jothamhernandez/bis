b00104b2401beb53af39b6436dc938df0967214b
Jotham Hernandez <jotham_hernandez@hotmail.com>
2018-10-21 19:14:54 +0800

    first commit

---
+++ database/migrations/2018_10_21_104815_create_members_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMembersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('members', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('first_name');
+            $table->string('middle_name')->nullable();
+            $table->string('last_name');
+            $table->enum('gender',['male','female']);
+            $table->enum('civil_status',['single','married','widowed','separated']);
+            $table->date('birthdate');
+            $table->string('email')->nullable();
+            $table->string('mobile')->nullable();
+            $table->string('landline')->nullable();
+            $table->enum('membership_status',['active','inactive']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('members');
+    }
+}
